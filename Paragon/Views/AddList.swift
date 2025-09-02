57803921eb55be127f88f028962a2c9c2c70b154
hizb <hizbullahkhan887@gmail.com>
2025-09-02 11:24:47 +0500

    list views

---
+++ Paragon/Views/AddList.swift
@@ -0,0 +1,8 @@
+//
+//  AddList.swift
+//  Paragon
+//
+//  Created by arsal on 9/2/25.
+//
+
+import Foundation
---
+++ Paragon/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  Paragon
+//
+//  Created by arsal on 9/2/25.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
