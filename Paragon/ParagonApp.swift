a0751b2aaf5d82e1e5309d8858dc2c817168d06d
hizb <hizbullahkhan887@gmail.com>
2025-09-02 11:01:45 +0500

    Initial Commit

---
+++ Paragon/ParagonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParagonApp.swift
+//  Paragon
+//
+//  Created by arsal on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ParagonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParagonTests/ParagonTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParagonTests.swift
+//  ParagonTests
+//
+//  Created by arsal on 9/2/25.
+//
+
+import Testing
+@testable import Paragon
+
+struct ParagonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
